37e63680ae1952f794b6475137b64e7e030e4754
Kenializeth <134826781+Kenializeth@users.noreply.github.com>
2023-05-27 23:54:32 -0600

    Delete js directory

--- js/personalizado.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function () {            
-    if($(document).scrollTop()>10){                
-        $('.bg-warning ').addClass("banner-background");
-    }else{
-        $('.bg-warning ').removeClass("banner-background");
-    }
-})
